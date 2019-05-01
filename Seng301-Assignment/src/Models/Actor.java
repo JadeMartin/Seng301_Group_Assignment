8f5f1eff45ab1a73664a1e24cb6356bf28bcc0cd
Jade <jma361@uclive.ac.nz>
2019-05-02 00:25:38 +1200

    fixed messages

--- Seng301-Assignment/src/Models/Actor.java
+++ Seng301-Assignment/src/Models/Actor.java
@@ -23,61 +23,30 @@ public class Actor{
     }
 
     //--- Setters ---
-
-    /**
-     * Set the first name of an Actor
-     * @param firstName
-     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    /**
-     * Set the last name of an Actor
-     * @param lastName
-     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    /**
-     * Set the level of trust of an Actor
-     * @param levelOfTrust
-     */
     public void setLevelOfTrust(double levelOfTrust) {
         this.firstName = firstName;
     }
 
     //--- Getters ---
-
-    /**
-     * function to return the first name of an Actor
-     * @return
-     */
     public String getFirstName() {
         return firstName;
     }
 
-    /**
-     * function to return the last name of an Actor
-     * @return
-     */
     public String getLastName() {
         return lastName;
     }
 
-    /**
-     * function to return the level of trust of an Actor
-     * @return
-     */
     public double getLevelOfTrust() {
         return levelOfTrust;
     }
 
-    /**
-     * function to return the actor id of an Actor
-     * @return
-     */
     public int getActorId() {
         return actorId;
     }
--- Seng301-Assignment/src/Models/Organisation.java
+++ Seng301-Assignment/src/Models/Organisation.java
@@ -13,27 +13,15 @@ public class Organisation {
     }
 
     //--- Setters ---
-    /**
-     * Set the name of an Organisation
-     * @param name
-     */
     public void setOrganisationName(String name) {
         this.name = name;
     }
 
     //--- Getters ---
-    /**
-     * function to return the name of an Organisation
-     * @return
-     */
     public String getOrganisationName() {
         return name;
     }
 
-    /**
-     * function to return the Organisation id of an Organisation
-     * @return
-     */
     public int getOrganisationIdId() {
         return organisationId;
     }
